cd8f8becf1b0d5aeb969997e800b3d0ceb33154c
AllanRanier <allanraier18@gmail.com>
2021-03-10 14:33:12 -0300

    Usuario Model

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsuarioModel extends Model{
+
+    protected $table = 'usuario';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'nome',
+        'email',
+        'senha'
+    ];
+
+}

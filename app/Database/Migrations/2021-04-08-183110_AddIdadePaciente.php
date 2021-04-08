377a0bd776e4b93d0afd89c7adeac343123e2691
Lucas Meyble <lucasmeyble123@gmail.com>
2021-04-08 15:32:42 -0300

    migration idade paciente

---
+++ app/Database/Migrations/2021-04-08-183110_AddIdadePaciente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddIdadePaciente extends Migration
+{
+	public function up()
+	{
+		$fields = [
+			'idade' => [
+				'type' => 'INT',
+			]
+		];
+
+		$this->forge->addColumn('paciente', $fields);
+	}
+
+	public function down()
+	{
+		$this->forge->dropColumn('paciente', $fields);
+	}
+}
